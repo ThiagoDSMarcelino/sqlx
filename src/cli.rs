431a87d1e158959c9c0882087dba49e78542527c
Thiago S. Marcelino <thiagodsmarcelino@gmail.com>
2026-01-03 18:25:07 -0300

    feat: Add commands descriptions

--- src/cli.rs
+++ src/cli.rs
@@ -9,10 +9,12 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    #[command(about = "Manage database connections")]
     Conn {
         #[command(subcommand)]
         cmd: ConnCommands,
     },
+    #[command(about = "Execute SQL commands")]
     Run {
         connection_name: String,
         file_path: String,
@@ -21,13 +23,16 @@ pub enum Commands {
 
 #[derive(Subcommand)]
 pub enum ConnCommands {
+    #[command(about = "Add a new database connection")]
     Add {
         connection_name: String,
         connection_driver: String,
         dns: String,
     },
+    #[command(about = "Remove an existing database connection")]
     Remove {
         connection_name: String,
     },
+    #[command(about = "List all database connections")]
     List,
 }
